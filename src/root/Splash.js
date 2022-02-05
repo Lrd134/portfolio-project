31ccd3b03be470bae253fb5f775c726924d90a13
Lrd134 <larryc3200@gmail.com>
2022-02-05 10:09:22 -0500

    styled columns

--- src/root/Splash.js
+++ src/root/Splash.js
@@ -7,6 +7,11 @@ function Splash() {
           <h1> Welcome to Gaming Decoded! </h1>
         </div>
         <p className="photo-attributes">Photo by <a href="https://unsplash.com/@_everaldo?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Everaldo Coelho</a> on <a href="https://unsplash.com/@_everaldo?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a></p>
+        <div className="three-columns">
+          <div className="social-media-box">Please check my socials!</div>
+          <div className="portfolio-box">This will have portfolio info</div>
+          <div className="etc-box">etc</div>
+        </div>
       </div>
     </div>
   )
