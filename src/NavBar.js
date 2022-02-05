e17f9d63ce41657a2c82739da19d59db871bd409
Lrd134 <larryc3200@gmail.com>
2022-02-05 05:59:04 -0500

    added navbar

---
+++ src/NavBar.js
@@ -0,0 +1,50 @@
+import React from 'react';
+
+function NavBar() {
+  return (
+    <div>
+        <a
+          className="linkedin-link"
+          href="https://www.linkedin.com/in/chesbro-jr/"
+          alt="linkedin"
+        >
+          LinkedIn
+        </a>
+        <a
+          className="instagram-link"
+          href="https://www.instagram.com/Lrd134/"
+          alt="instagram"
+        >
+          Instagram
+        </a>        
+        <a
+          className="blog-link"
+          href="https://blog.gamingdecoded.org/"
+          alt="blog"
+        >
+          Blog
+        </a>
+        <a
+          className="Twitter-link"
+          href="https://twitter.com/GamingDecoded"
+          alt="twitter"
+        >
+          🐦 Twitter 🐦
+        </a>
+        <a
+          className="minecraft-link"
+          href="https://gamingdecoded.org/minecraft"
+          alt="minecraft"
+        >
+          Minecraft
+        </a>
+        <a
+          className="portfolio-link"
+          href="https://gamingdecoded.org/portfolio"
+          alt="portfolio"
+        >
+          Portfolio
+        </a>
+    </div>
+  )
+}
