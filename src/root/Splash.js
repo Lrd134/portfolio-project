4c60da1843217035dff90770cfe93d770a514e26
Lrd134 <larryc3200@gmail.com>
2022-02-05 09:12:21 -0500

    added background image

--- src/root/Splash.js
+++ src/root/Splash.js
@@ -1,9 +1,13 @@
 import React from 'react';
-
 function Splash() {
   return (
     <div>
-      Welcome to Gaming Decoded!
+      <div className="splash">
+        <div>
+          <h1> Welcome to Gaming Decoded! </h1>
+        </div>
+        <p className="photo-attributes">Photo by <a href="https://unsplash.com/@_everaldo?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Everaldo Coelho</a> on <a href="https://unsplash.com/@_everaldo?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a></p>
+      </div>
     </div>
   )
 }
