1deb936077cd6df03f8668eec52ddc939db3954d
Lrd134 <larryc3200@gmail.com>
2022-02-05 06:04:16 -0500

    setup redux store,  devtools, react router, and thunk

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
-import './App.css';
-import NavBar from 'NavBar';
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <NavBar/>
-      </header>
-    </div>
-  );
-}
 
+import './App.css';
+import React, { Component } from 'react';
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+import { Provider } from 'react-redux';
+import RootReducer from './root/RootReducer'
+import {
+  BrowserRouter as Router,
+  Routes } from "react-router-dom";
+import AppRoutes from './root/routes.js'
+// ...
+const store = compose(
+    applyMiddleware(thunk),
+    window.__REDUX_DEVTOOLS_EXTENSION__
+    ? window.__REDUX_DEVTOOLS_EXTENSION__()
+    : f => f,
+)(createStore)(RootReducer);
+
+class App extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <Router>  
+          <Routes>
+              {AppRoutes()}
+          </Routes>
+        </Router>
+    </Provider>
+
+
+    );
+  }
+}
 export default App;
+
