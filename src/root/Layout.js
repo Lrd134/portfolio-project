e6c0c5c6782756fbdf28c8dfd5e6efa20558d481
Lrd134 <larryc3200@gmail.com>
2022-02-05 08:06:34 -0500

    added outlet to layout

--- src/root/Layout.js
+++ src/root/Layout.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Outlet } from 'react-router-dom';
 import NavBar from './NavBar';
 function Layout() {
   return (
     <div>
       <NavBar />
+      <Outlet />
     </div>
   )
 }
