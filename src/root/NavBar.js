4af95743f64f029a4d4e74d5e2067db0a737e382
Lrd134 <larryc3200@gmail.com>
2022-02-05 09:38:19 -0500

    updated navbar

--- src/root/NavBar.js
+++ src/root/NavBar.js
@@ -3,57 +3,55 @@ import React from 'react';
 function NavBar() {
   return (
     <div className="navbar">
-      <div className="centering">
-        {/* <a
-          className="linkedin link-layout"
-          href="https://www.linkedin.com/in/chesbro-jr/"
-          alt="linkedin"
-        >
-          LinkedIn
-        </a> */}
-        {/* <a
-          className="instagram link-layout"
-          href="https://www.instagram.com/Lrd134/"
-          alt="instagram"
-        >
-          Instagram
-        </a>         */}
-        <a
-          className="blog link-layout"
-          href="https://blog.gamingdecoded.org/"
-          alt="blog"
-        >
-          Blog
-        </a>
-        <a
-          className="forums link-layout"
-          href="https://gamingdecoded.org/forums"
-          alt="forums"
-        >
-          Forums
-        </a>
-        {/* <a
-          className="Twitter link-layout"
-          href="https://twitter.com/GamingDecoded"
-          alt="twitter"
-        >
-          🐦 Twitter 🐦
-        </a> */}
-        <a
-          className="minecraft link-layout"
-          href="https://gamingdecoded.org/minecraft"
-          alt="minecraft"
-        >
-          Minecraft
-        </a>
-        <a
-          className="portfolio link-layout"
-          href="https://gamingdecoded.org/portfolio"
-          alt="portfolio"
-        >
-          Portfolio
-        </a>
-      </div>
+      {/* <a
+        className="linkedin link-layout"
+        href="https://www.linkedin.com/in/chesbro-jr/"
+        alt="linkedin"
+      >
+        LinkedIn
+      </a> */}
+      {/* <a
+        className="instagram link-layout"
+        href="https://www.instagram.com/Lrd134/"
+        alt="instagram"
+      >
+        Instagram
+      </a>         */}
+      <a
+        className="blog link-layout"
+        href="https://blog.gamingdecoded.org/"
+        alt="blog"
+      >
+        Blog
+      </a>
+      <a
+        className="forums link-layout"
+        href="https://gamingdecoded.org/forums"
+        alt="forums"
+      >
+        Forums
+      </a>
+      {/* <a
+        className="Twitter link-layout"
+        href="https://twitter.com/GamingDecoded"
+        alt="twitter"
+      >
+        🐦 Twitter 🐦
+      </a> */}
+      <a
+        className="minecraft link-layout"
+        href="https://gamingdecoded.org/minecraft"
+        alt="minecraft"
+      >
+        Minecraft
+      </a>
+      <a
+        className="portfolio link-layout"
+        href="https://gamingdecoded.org/portfolio"
+        alt="portfolio"
+      >
+        Portfolio
+      </a>
     </div>
   )
 }
