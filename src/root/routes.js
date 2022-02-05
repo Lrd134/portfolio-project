e82978f83e93ef821625bbdc53984bf15e7b24dd
Lrd134 <larryc3200@gmail.com>
2022-02-05 06:10:00 -0500

    added route handler

---
+++ src/root/routes.js
@@ -0,0 +1,18 @@
+import { Route } from "react-router-dom";
+import Splash from 'Splash.js'
+const myRoutes = () => {
+  return (
+    <Route path="/" element={<Splash />} >
+      <Route
+        path="*"
+        element={
+          <main style={{ padding: "1rem" }}>
+            <p>There's nothing here!</p>
+          </main>
+        }
+      />
+    </Route>
+  )
+}
+
+export default myRoutes;
