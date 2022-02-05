be7fc23946b6d624a9c43fa227569f9096234ed8
Lrd134 <larryc3200@gmail.com>
2022-02-05 06:13:46 -0500

    added user reducer

---
+++ src/users/UserReducer.js
@@ -0,0 +1,11 @@
+const UserReducer = (state = {
+  users: [],
+  requesting: false,
+}, action) => {
+  switch(action.type) {
+    default:
+      return state;
+  }
+}
+
+export default UserReducer
