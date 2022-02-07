c5c74eaba7d698bb18b37126bb278088e753bbef
Lrd134 <larryc3200@gmail.com>
2022-02-07 06:38:38 -0500

    added other navbar

--- src/root/Layout.js
+++ src/root/Layout.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import NavBar from './NavBar';
-function Layout() {
-  return (
-    <div>
-      <NavBar />
-      <Outlet />
-    </div>
-  )
+function Layout(props = {
+  type: "main"
+}) {
+
+  if (props.type === "main") {
+    return (
+      <div>
+        <NavBar />
+        <Outlet />
+      </div>
+    )
+  } else if (props.type === "hover") {
+    return (
+      <div>
+        <NavBar />
+      </div>
+    )
+  }
 }
 
 export default Layout;
