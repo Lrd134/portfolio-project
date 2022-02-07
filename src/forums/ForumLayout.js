a23f260733aa645b2888474bc51314def457fe4d
Lrd134 <larryc3200@gmail.com>
2022-02-07 06:35:32 -0500

    shows home when options clicked

--- src/forums/ForumLayout.js
+++ src/forums/ForumLayout.js
@@ -1,19 +1,30 @@
-import React from "react";
+import React, { Component } from "react";
 import { Link, Outlet } from "react-router-dom";
+import Layout from "../root/Layout";
 
-function ForumLayout() {
-  return (
-    <div className="forum-layout">
-      <Link to="/forums">Home</Link>
-      <Link to="/forums/login">Login</Link>
-      <div className="options">
-        <div className="options-style"></div>
-        <div className="options-style"></div>
-        <div className="options-style"></div>
+class ForumLayout extends Component {
+  state = {
+    showOptions: false
+  }
+
+  render() {
+    const showOptions = () => {
+      this.setState({showOptions: !this.state.showOptions})
+    }
+    return (
+      <div className="forum-layout">
+        <Link to="/forums">Home</Link>
+        <Link to="/forums/login">Login</Link>
+        <div onClick={showOptions} className="options">
+          <div className="options-style"></div>
+          <div className="options-style"></div>
+          <div className="options-style"></div>
+        </div>
+        {this.state.showOptions ? <Layout type="hover" /> : null }
+        <Outlet/>
       </div>
-      <Outlet/>
-    </div>
-  )
+    )
+  }
 }
 
 export default ForumLayout;
