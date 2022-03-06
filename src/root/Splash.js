5bffe4bc10e0e8981354757208aa53d57ea7ec8e
Lrd134 <larryc3200@gmail.com>
2022-03-06 07:07:41 -0500

    720p splash done

--- src/root/Splash.js
+++ src/root/Splash.js
@@ -25,7 +25,7 @@ function Splash() {
         <h6 id="ps">Gaming Decoded is intended to be an organization that will help teach video game development. Created by Lawrence Chesbro, Jr. he plans to use this website to showcase his capabilties.</h6>
         
         <YoutubeEmbed embedId="8cWGCMXNhZw" />
-        <div className="photo-left-caption"><h3>Current Goal:<h2>Frontend React Position</h2>Languages:<h2>
+        <div className="photo-left-caption"><h3>Current Goal:<h2>Frontend React Position</h2><span className="right-lowres">Languages:<h2>
           <a rel="noreferrer" href="https://ruby-doc.org/" alt="Ruby Documentation" target="_blank"><img height="60px" width="60px" src={rubyIco} alt="Ruby Programming Language"/></a>
           <a rel="noreferrer" href="https://guides.rubyonrails.org/" alt="Rails Documentation" target="_blank"><img height="60px" width="60px" src={railsIco} alt="Ruby on Rails Framework"/></a>
           <a style={{
@@ -33,7 +33,7 @@ function Splash() {
             "text-decoration": "none"
           }} rel="noreferrer" href="https://git-scm.com/doc" alt="Git Documentation" target="_blank"><img height="60px" width="60px" src={gitIco} alt="Git CLI"/> </a>
           <a rel="noreferrer" href="https://reactjs.org/docs/getting-started.html" alt="React Documentation" target="_blank"><img height="60px" width="60px" src={reactIco} alt="React Framework"/></a><br></br>
-          <a rel="noreferrer" href="https://developer.mozilla.org/" alt="MDN Web Documentation" target="_blank"><img height="90px" width="180px" src={webDevIcos} alt="HTML5, CSS, and JavaScript ES6"/></a> </h2></h3></div>
+          <a rel="noreferrer" href="https://developer.mozilla.org/" alt="MDN Web Documentation" target="_blank"><img height="90px" width="180px" src={webDevIcos} alt="HTML5, CSS, and JavaScript ES6"/></a> </h2></span></h3></div>
         <img className="photo-left" src={selfPortrait} alt="Lawrence Chesbro, Jr. looking off in the distance towards Java Lake."></img>
 
         <div className="three-columns">
