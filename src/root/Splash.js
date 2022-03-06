a2f9acfcc9604df1e5b6b697ea6b5bf23c9a619c
Lrd134 <larryc3200@gmail.com>
2022-03-06 10:00:58 -0500

    added s9+ css

--- src/root/Splash.js
+++ src/root/Splash.js
@@ -24,7 +24,8 @@ function Splash() {
         <h6 id="ps">Gaming Decoded is intended to be an organization that will help teach video game development. Created by Lawrence Chesbro, Jr. he plans to use this website to showcase his capabilties.</h6>
         
         <YoutubeEmbed embedId="8cWGCMXNhZw" />
-        <div className="photo-left-caption"><h3>Current Goal:<h2>Frontend React Position</h2><span className="right-lowres">Languages:<h2>
+        <div className="photo-left-caption"><h3><span className="right-s9">Current Goal:</span><h2>Frontend React Position</h2><span className="right-lowres right-s9">Languages:<h2>
+          <span className="img-right-s9">
           <a rel="noreferrer" href="https://ruby-doc.org/" alt="Ruby Documentation" target="_blank"><img height="60px" width="60px" src={rubyIco} alt="Ruby Programming Language"/></a>
           <a rel="noreferrer" href="https://guides.rubyonrails.org/" alt="Rails Documentation" target="_blank"><img height="60px" width="60px" src={railsIco} alt="Ruby on Rails Framework"/></a>
           <a style={{
@@ -32,7 +33,7 @@ function Splash() {
             "text-decoration": "none"
           }} rel="noreferrer" href="https://git-scm.com/doc" alt="Git Documentation" target="_blank"><img height="60px" width="60px" src={gitIco} alt="Git CLI"/> </a>
           <a rel="noreferrer" href="https://reactjs.org/docs/getting-started.html" alt="React Documentation" target="_blank"><img height="60px" width="60px" src={reactIco} alt="React Framework"/></a><br></br>
-          <a rel="noreferrer" href="https://developer.mozilla.org/" alt="MDN Web Documentation" target="_blank"><img height="90px" width="180px" src={webDevIcos} alt="HTML5, CSS, and JavaScript ES6"/></a> </h2></span></h3></div>
+          <a rel="noreferrer" href="https://developer.mozilla.org/" alt="MDN Web Documentation" target="_blank"><img height="90px" width="180px" src={webDevIcos} alt="HTML5, CSS, and JavaScript ES6"/></a> </span></h2></span></h3></div>
         <img className="photo-left" src={selfPortrait} alt="Lawrence Chesbro, Jr. looking off in the distance towards Java Lake."></img>
 
         <div className="three-columns">
