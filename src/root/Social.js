6b9c392e1812dff31a2639918ada2255ea7cbef1
Lrd134 <larryc3200@gmail.com>
2022-03-06 03:23:50 -0500

    linked open new tab

--- src/root/Social.js
+++ src/root/Social.js
@@ -5,7 +5,7 @@ function Social(props) {
   return (
     <div className={`social ${props.social}`}>
       {props.social}<br></br>
-      <a href={props.url}><img src={props.icon} alt={props.handle}></img></a><br></br>
+      <a href={props.url} target="_blank"><img src={props.icon} alt={props.handle}></img></a><br></br>
     </div>
   )
 }
