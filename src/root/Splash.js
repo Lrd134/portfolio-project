7c1d2feca82a892a7a19b40447cada5e8f765299
Lrd134 <larryc3200@gmail.com>
2022-04-06 16:32:13 -0400

    hid socials

--- src/root/Splash.js
+++ src/root/Splash.js
@@ -10,26 +10,33 @@ import reactIco from '../media/icons/react-icon.png';
 import webDevIcos from '../media/icons/webdev-icons.png';
 import gitIco from '../media/icons/git-icon.png';
 import selfPortrait from '../me_temp.jpg';
-import React from 'react';
+import React, { useState } from 'react';
 import Social from './Social';
 import YoutubeEmbed from './YoutubeEmbed';
 import './splash.css';
 function Splash() {
+  const [socialClicked, setSocialClicked] = useState(false)
+  const changeSocialClicked = event => {
+    setSocialClicked(!socialClicked);
+  }
   return (
     <div>
       <div className="splash">
         <div className="three-columns">
+          
           <div className="social-media-box">
-            <div className="socials">
+            {socialClicked ?
+            <div className="socials" onClick={changeSocialClicked}>
               <Social url="https://www.linkedin.com/in/chesbro-jr/" social="LinkedIn" altTxt="Lawrence Chesbro Jr's LinkedIn" icon={linkedInIco} />
-              <Social url="https://blog.gamingdecoded.org/" social="Blog" altTxt="Lawrence Chesbro Jr's Blog" icon={blogIco}/>
+              <Social url="https://blog.gamingdecoded.org/" social="Blog" altTxt="Lawrence Chesbro Jr's Blog" icon={blogIco}/><br></br>
               <Social url="https://twitter.com/GamingDecoded" social="Twitter" altTxt="@GamingDecoded on Twitter" icon={twitterIco}/>
               <Social url="https://www.youtube.com/channel/UCNqDMQ-o3_iGszqxdDq_ldA" social="YouTube" altTxt="Lawrence Chesbro Jr' YouTube Channel" icon={youTubeIco} />
-            </div>
+            </div> : <div className="hidden" onClick={changeSocialClicked}></div>
+          }
           </div>
           <div className="portfolio-box">
             <Social url="https://lrd134.github.io/basic-game/" social="Basic Game" altTxt="Play a Basic Game Here" icon={docIco} />
-            <Social url="https://gdc-forums.herokuapp.com/" social="Mock Forums" altTxt="Explore the Forums Here" icon={docIco} />
+            <Social url="https://gdc-forums.herokuapp.com/" social="Mock Forums" altTxt="Explore the Forums Here" icon={docIco} /><br></br>
             <Social url="https://youtu.be/8cWGCMXNhZw" social="Mock Exchange" altTxt="Watch a Demo Here" icon={docIco} />
             <Social url="https://github.com/Lrd134" social="GitHub" altText="Lawrence Chesbro Jr's GitHub" icon={gitHubIco} />
           </div>
