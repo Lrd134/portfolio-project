f8e64954aa3c45e014e1cb6361bb6762e74c54b4
Lrd134 <larryc3200@gmail.com>
2022-05-12 17:14:07 -0400

    split up categories

--- src/forums/ForumContainer.js
+++ src/forums/ForumContainer.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import CatGroup from './CatGroup';
 import Category from './Category';
+import './forums.css';
 function ForumContainer() {
   return (
     <div>
       <h1>Welcome to the forums!<br></br>
       Construction to be finished May 31st.</h1>
-      <CatGroup key="general" title="general">  
+      <CatGroup key="general" title="General">  
         <Category key="ot" title="Off Topic"/>
         <Category key="intros" title="Introductions"/>
         <Category key="fd" title="Friendly Discussions"/>
       </CatGroup>
-      <CatGroup key="programming" title="programming"/>
+      <CatGroup key="programming" title="Programming"/>
 
     </div>
   )
